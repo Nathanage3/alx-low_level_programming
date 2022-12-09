0bb71029bc4e7fa0499c2d8c7b5dd45a058a3ae5
Nathanage3 <sisay954@gmail.com>
2022-12-09 08:11:04 -0800

    c coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,5 @@
 */
 
 int main(void)
-{  
-	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);}
