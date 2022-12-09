cb2b5e3ca53844f3e9407a78b4ac7b070886440c
Nathanage3 <sisay954@gmail.com>
2022-12-08 18:37:53 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,5 @@
-#include <stdio.h> 
-int main(void) { puts("Programming is like building a multilingual puzzle\n") ; return(0) ; }
+#include <stdio.h>
+
+
+
+int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
