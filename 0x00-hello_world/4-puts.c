8bb0d723cd14c3a24c9625a1254785028f095008
Nathanage3 <sisay954@gmail.com>
2022-12-09 07:25:10 -0800

    MMY  first C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/**
- * main - Entry
- * Return : Always 0
+/*
+ * main - print a line of code using puts
+ *
+ * Return : 0
 */
 
 int main(void)
 {  
-       	puts("Programming is like building a multilingual puzzle\n");
+       	puts("\"Programming is like building a multilingual puzzle\n");
        	return (0);
 }
