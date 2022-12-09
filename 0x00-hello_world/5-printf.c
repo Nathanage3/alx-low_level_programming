fdcb4e30f4a9a16d2ed2d1f505ca31c95dd716d7
Nathanage3 <sisay954@gmail.com>
2022-12-09 11:07:12 -0800

    5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,5 +7,7 @@
  */
 
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0); } 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+} 
