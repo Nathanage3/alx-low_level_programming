e23c427fa71e2faf1b67eb3ef3a344b3e25dbc54
Nathanage3 <sisay954@gmail.com>
2022-12-10 04:06:22 -0800

    6   Size in bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,11 @@
 int main(void)
 {
 	char a;
-
 	int b;
-
 	long int c;
-
 	long long int d;
-
 	float e;
-
+	
 	printf("Size of a char: %lu byte(S)\n",	(unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
