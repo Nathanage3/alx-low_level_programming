a1e1a180f1dfc1329cac17774ea5b4cf20ce92f2
Nathanage3 <sisay954@gmail.com>
2022-12-10 06:24:51 -0800

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main -main block
+ *
+ * Return: 0
+ */
+
+int main(void)
+{write("2, and that piece of art is useful\" - Dora Korpar, 2015-10-19, 59");
+return (1);	}
