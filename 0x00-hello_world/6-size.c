c65063e5908bec3c52e6ba6ba00bd2682ad83839
Nathanage3 <sisay954@gmail.com>
2022-12-10 04:43:26 -0800

    Size in bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
-{#include <stdio.h>
+#include <stdio.h>
+
 /**
  * main - main block
  *
  * Return: 0
  */
+
 int main(void)
 {
-
 	printf("Size of a char: %lu byte(S)\n",	sizeof(char));
 	printf("Size of an int: %lu byte(S)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(S)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(S)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(S)\n", sizeof(float));
 	return	(0);
-
-}}
+}
