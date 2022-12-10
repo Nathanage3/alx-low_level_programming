c6961e3c04d494db73eb15ebe96e6762f84e613d
Nathanage3 <sisay954@gmail.com>
2022-12-10 02:55:01 -0800

    6   Size in bytes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char	a;
+
+	int	b;
+
+	long int	c;
+
+	long long int	d;
+
+	float	e;
+
+	printf("%a Size of a char:\n",	a);
+	printf("%b Size of an int:\n",	b);
+	printf("%c Size of a long int\n",	c);
+	printf("%d Size of a long long int\n",	d);
+	printf("%e Size of a float\n",	e);
+	return	(0);
+
+}
