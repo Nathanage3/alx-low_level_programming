ae8dbd9f0af4666fb154f91cb0bd85f35bb5c940
Nathanage3 <sisay954@gmail.com>
2022-12-12 15:27:47 -0800

    no

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+
+	d = 'A';
+	while	
+		(c <= 'z')	{
+			putchar(c);
+			c++;
+		}
+	while	
+		(d <= 'Z')	{
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
