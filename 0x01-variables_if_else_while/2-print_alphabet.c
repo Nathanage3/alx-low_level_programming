285984b79a93bfa734258d748f0eceae36e6652c
Nathanage3 <sisay954@gmail.com>
2022-12-12 14:14:14 -0800

    lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main 
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	
+	c = 'a';
+	while
+		(c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+			
+}
