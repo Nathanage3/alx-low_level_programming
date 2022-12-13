5e50c743beb573b392d8fcdeaf82b9a977f5202e
Nathanage3 <sisay954@gmail.com>
2022-12-12 16:53:55 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int c;
+	c = 0;
+	while
+
+		(c < 10) {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
