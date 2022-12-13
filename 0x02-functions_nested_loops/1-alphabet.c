8b86bf4a47f8c8e4160845770ac0a097c961862d
Nathanage3 <sisay954@gmail.com>
2022-12-13 04:55:43 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet - prints lower case letters
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
