cee17855338bf6b4b45cbf1b1d67ae80738515c0
Nathanage3 <sisay954@gmail.com>
2022-12-12 18:21:01 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	while
+	       (d < 10) {
+	       putchar(d + '0');
+	       d++;	}
+	while
+	       (c <= 'f') {
+	       putchar(c);
+	       c++;	}
+	putchar('\n');
+	return (0);
+}
+
