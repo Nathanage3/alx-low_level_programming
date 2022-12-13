18f1f5d798753bdaa7d44cd8ec5d4234493b2dbc
Nathanage3 <sisay954@gmail.com>
2022-12-13 10:59:01 -0800

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,31 @@
-#include <unistd.h>
+#ifndef MAIN_H
 
-/**
- *
- * * _putchar - writes the character c to stdout
- *
- * * @c: The character to print
- *
- * *
- *
- * * Return: On success 1.
- *
- * * On error, -1 is returned, and errno is set appropriately.
- *
- * */
+#define MAIN_H
 
-int _putchar(char c)
+int _putchar(char);
 
-{
+void print_alphabet(void);
 
-	return (write(1, &c, 1));
-}
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+#endif
