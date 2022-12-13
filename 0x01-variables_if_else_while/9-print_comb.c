6e7436f5c062c01d0c22ed3792733fa7799a758d
Nathanage3 <sisay954@gmail.com>
2022-12-12 18:53:54 -0800

    10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+		       putchar(',');
+		       putchar(' ');	}
+	}
+	putchar('\n');
+	return (0);
+}
+
