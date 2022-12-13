47098169145e21631333dcb2fb0dc02ddfaec34b
Nathanage3 <sisay954@gmail.com>
2022-12-12 17:57:46 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0;
+ *
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c <= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
