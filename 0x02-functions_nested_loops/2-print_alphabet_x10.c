8bbb32a6d04fdec5b0fb45cc5a8d3a8a6801e3a2
Nathanage3 <sisay954@gmail.com>
2022-12-13 13:53:11 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,13 @@ void print_alphabet_x10(void)
 	char c;
 
 	int a;
-
 	
 	for (a = 0; a < 10; a++)
 	{
-		_putchar('\n');
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 	}
+	_putchar('\n');
 }
