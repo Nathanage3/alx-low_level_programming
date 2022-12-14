ebadcf3e3eb860f25aa7b8d422b9d526a1ccb234
Nathanage3 <sisay954@gmail.com>
2022-12-14 11:53:30 -0800

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,11 +6,11 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a <= 2; a++)
+		for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 9; b++)
+			for (b = 0; b <= 9; b++)
 		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+				if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 					{
 					for (c = 0; c <= 5; c++)
 					{
