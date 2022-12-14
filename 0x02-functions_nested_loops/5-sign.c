e4cd0e22a8423dd4eb570cfaf0ce229342ceac58
Nathanage3 <sisay954@gmail.com>
2022-12-13 17:23:36 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Chechs the sign of a number
+ * @c - sign of the number to be printed
+ * Return: 1 for + sign,  0 for 0 and -1 for -sign
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
