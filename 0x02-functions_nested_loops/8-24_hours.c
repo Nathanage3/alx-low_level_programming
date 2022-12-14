cad91946989d2a6de2433dea4dca4274cccfc7c2
Nathanage3 <sisay954@gmail.com>
2022-12-14 11:34:39 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minutes of the day
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+					{
+					for (c = 0; c <= 5; c++)
+					{
+					for (d = 0; d <= 9; d++)
+					{
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(58);
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
+					}
+					}
+					}
+					}
+					}
+					}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    jack_bauer();
+
+	        return (0);
+
+}
