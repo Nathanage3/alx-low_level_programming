c3c72943b57ee48d0bfeb0203c2543aecb2964b7
Nathanage3 <sisay954@gmail.com>
2022-12-15 13:24:10 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,9 +5,11 @@
  * Return: 0
  */
 
+
 int main(void)
 
 {
+
 	int i;
 
 	printf("Infinite loop incoming :(\n");
@@ -18,4 +20,5 @@ int main(void)
 	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
+
 }
