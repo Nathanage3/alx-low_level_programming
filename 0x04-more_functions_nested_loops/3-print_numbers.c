50f4f05e3f56d2e393c63238ff9d28118ac38365
Nathanage3 <sisay954@gmail.com>
2022-12-15 18:21:22 -0800

    3-print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Return: empty
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar("%d\n",  i);
+	}
+}
