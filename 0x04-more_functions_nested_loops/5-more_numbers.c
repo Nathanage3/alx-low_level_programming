5d6a9225466434a3cdc04963558ed3b7ff891db7
Nathanage3 <sisay954@gmail.com>
2022-12-16 04:35:28 -0800

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
-	
+	for (i = 0; i < 10; i++)
 	{
-		for(j = 0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 14)
+			if (j >= 10)
 				_putchar('1');
 			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
+
 	}
 }
-		
-	
