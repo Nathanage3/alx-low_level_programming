991b7ec5c5cd4e6dd5ff546c0484224299785156
Nathanage3 <sisay954@gmail.com>
2022-12-19 15:26:37 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/***
+ * void reset_to_98 - reset n to 98
+ * @n: the integer to executed
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    char *str;
+
+
+
+	        str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+		    _puts(str);
+
+		        return (0);
+
+}
