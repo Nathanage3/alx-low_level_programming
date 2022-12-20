cb48a64559eeff6d9459ebd7191a5f8f03f8a55a
Nathanage3 <sisay954@gmail.com>
2022-12-19 17:34:58 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints the reverse of the input string
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	
+	while(i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
