6058c5614e62c1772700dc1bc40a2097a7f4f027
Nathanage3 <sisay954@gmail.com>
2022-12-19 18:17:26 -0800

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - puts
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
