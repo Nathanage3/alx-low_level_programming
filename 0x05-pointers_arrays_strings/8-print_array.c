c7d692c971ca988a37abeac3c717c76b025153b6
Nathanage3 <sisay954@gmail.com>
2022-12-20 16:09:22 -0800

    8-print

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,16 +7,15 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < n - 1)
+		if (i != (n - 1))
 		{
 			printf(", ");
 		}
 	}
-	
-	_putchar('\n');
+	printf("\n");
 }
