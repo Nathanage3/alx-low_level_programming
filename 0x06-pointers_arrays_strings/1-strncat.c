8d98f9358bebb18b5e0f0f90ff0e2c15603c71c3
Nathanage3 <sisay954@gmail.com>
2022-12-23 15:58:15 -0800

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+/**
+ * _strncat - concatinate n characters of src to dest
+ * @dest: destination
+ * @src: to be appended
+ * @n: n characters
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	while (j < n)
+		dest[i] = '\0';
+	return (dest);
+}
