a25c9486a0ce92450b2f80d62bc9a9a2fd2ac4ec
Nathanage3 <sisay954@gmail.com>
2022-12-23 16:57:51 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - comparing string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: Always 0
+ */
+
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] = '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			s[i] - s2[i];
+		}
+		i++;
+	}
+	return (0);
