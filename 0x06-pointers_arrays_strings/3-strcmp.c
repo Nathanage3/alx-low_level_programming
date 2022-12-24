ef48fb99e1c8f8766b28aed27fb44793298c26e3
Nathanage3 <sisay954@gmail.com>
2022-12-23 17:08:55 -0800

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] != '\0' && s2[i] = '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			s[i] - s2[i];
+		return	s1[i] - s2[i];
 		}
 		i++;
 	}
 	return (0);
+}
