1477f096926b942ec05b4c835d60d0bb6ad55123
Nathanage3 <sisay954@gmail.com>
2022-12-26 04:46:23 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,12 @@ int main(void)
 	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
+
 		if (a < 9)
 		{
-		       putchar(',');
-		       putchar(' ');	}
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
