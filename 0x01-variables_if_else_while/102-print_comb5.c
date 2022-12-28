d3f6b1f67d898b3bdc6fa9376cd532717d5b244b
Nathanage3 <sisay954@gmail.com>
2022-12-28 05:39:34 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
