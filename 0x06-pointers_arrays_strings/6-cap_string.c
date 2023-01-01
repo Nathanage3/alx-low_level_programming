14f81c375f0f9746b2b4a23d4569b36aba4b3dd3
Nathanage3 <sisay954@gmail.com>
2023-01-01 12:17:37 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * cap_string- cap stirng
+ * @s: sstring
+ * Return:char value
+ */
+
+char *cap_string(char *s)
+{
+	int a = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[a])
+	{
+		i = 0;
+			while (i < cspc)
+			{
+				if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+					s[a] -= 32;
+				i++;
+			}
+		a++;
+	}
+	return (s);
+}
