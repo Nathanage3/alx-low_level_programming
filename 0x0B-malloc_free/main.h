2332502617d6e8b8233b3ca12d2abdd52063dcdf
Nathanage3 <sisay954@gmail.com>
2023-01-11 06:24:18 -0800

    0x0B-malloc_free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,4 +8,5 @@ char *str_concat(char *s1, char *s2);
 char *_strdup(char *str); 
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
+void simple_print_buffer(char *buffer, unsigned int size);
 #endif
