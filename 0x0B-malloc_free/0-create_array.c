31e00a44a58dd4b4025e7b22c1ccd70bd0bbd54e
Nathanage3 <sisay954@gmail.com>
2023-01-11 06:17:40 -0800

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_array - array of size
+ * @size: size of the array
+ * @c: character to be executed
+ * Return: char value
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array = (char *)malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (unsigned int i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
+void free_grid(int **grid, int height);   
+int **alloc_grid(int width, int height);  
+char *str_concat(char *s1, char *s2);  
+char *_strdup(char *str); 
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+#endif
