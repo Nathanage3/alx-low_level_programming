9425f92cbdfe61f49038a8a49159f212455bf1b7
Nathanage3 <sisay954@gmail.com>
2023-01-12 14:50:49 -0800

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,9 +2,8 @@
 #define MAIN_H
 int _putchar(char c);   
 void *malloc_checked(unsigned int b); 
-char *string_nconcat(char *s1, char *s2vi main.h, unsigned int n); 
-void *_calloc(unsigned int nmemb, unsigned int size);int *array_range(int min, int max); 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size); 
-void simple_print_buffer(char *buffer, unsigned int size); 
-void simple_print_buffer(int *buffer, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n); 
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max); 
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
