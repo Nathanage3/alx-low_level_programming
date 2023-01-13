9585a2e7e4b4a2685c3cb0a6b47e85f9c95cfcdd
Nathanage3 <sisay954@gmail.com>
2023-01-12 17:09:45 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,6 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n); 
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max); 
+char *_memcpy(char *dest, char *src, unsigned int n);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
