2242d0915e4b0e2b2a4dbb76d4fc7604aea844e5
Nathanage3 <sisay954@gmail.com>
2023-05-18 15:41:39 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
