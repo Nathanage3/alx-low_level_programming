30ffd1d2ca1dd3776d58b59e7c41551376b18cbb
Nathanage3 <sisay954@gmail.com>
2023-05-18 15:29:02 -0700

    puts in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - the main function.
+ * Return: 0 for success 1 for program fail
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
