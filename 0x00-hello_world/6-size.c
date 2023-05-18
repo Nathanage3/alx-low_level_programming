4da1083de40bec0bf4ef96ad70d7741640c8c32b
Nathanage3 <sisay954@gmail.com>
2023-05-18 16:24:49 -0700

    size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,6 @@
  * main - main function
  * Return: Always o for success
  */
-/*Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)*/
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
