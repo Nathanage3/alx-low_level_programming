f7a744f28878cf0b7a2dd1f4200563471549c688
Nathanage3 <sisay954@gmail.com>
2023-05-19 06:50:34 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Always 0
+ */
+int main()
+{
+	int c = 97;
+
+	while (c <= 122)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
