8400d0cdf31b57c7a17073f88b0486b8c6d10ea1
Nathanage3 <sisay954@gmail.com>
2023-05-19 15:41:49 -0700

    zero to nine decimal nubers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
