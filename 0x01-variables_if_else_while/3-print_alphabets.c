63c736db4b4b9192ae1d7f9e30b1e186a5ba85b6
Nathanage3 <sisay954@gmail.com>
2023-05-19 15:04:30 -0700

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,12 +5,16 @@
  */
 int main(void)
 {
-        int c;
+	int c;
 
         for (c = 97; c <= 122; c++)
-                putchar(c);
-        for (c = 65; c <= 90; c++)
+	{
 		putchar(c);
+	}
+        for (c = 65; c <= 90; c++)
+	{
+		putchar(c);
+	}
         putchar('\n');
         return (0);
 }
