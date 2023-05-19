eeeb6cbbff1532f1b674005d2177e33f42acefe8
Nathanage3 <sisay954@gmail.com>
2023-05-19 14:53:40 -0700

    lower and UPPER letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * Return: Always 0
+ */
+int main(void)
+{
+        int c;
+
+        for (c = 97; c <= 122; c++)
+                putchar(c);
+        for (c = 65; c <= 90; c++)
+		putchar(c);
+        putchar('\n');
+        return (0);
+}
