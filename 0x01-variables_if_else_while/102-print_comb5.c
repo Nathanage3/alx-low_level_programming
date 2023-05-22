2ff58d2417860e4e6457b299c2278a3fc638a594
Nathanage3 <sisay954@gmail.com>
2023-05-22 04:20:57 -0700

    print combination of numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i / 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j / 10) + '0');
+			if (i == 98 && j == 99)
+				break;
+					putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
