d7278c6a5b64a4f206b3a2cce99fe924a257c2d2
Nathanage3 <sisay954@gmail.com>
2023-05-22 04:35:19 -0700

    print combination of numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,10 +12,10 @@ int main(void)
 		for (j = i + 1; j < 100; j++)
 		{
 			putchar((i / 10) + '0');
-			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
 			putchar((j / 10) + '0');
-			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 			if (i == 98 && j == 99)
 			break;
 			putchar(',');
