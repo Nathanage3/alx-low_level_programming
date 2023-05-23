45cccf1f808ca4844493719a5efbc74dd207c9b3
Nathanage3 <sisay954@gmail.com>
2023-05-23 10:08:22 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
  * _islower - prints lower alphabets
- * @c: take c as a parameter
+ * @r: take c as a parameter
  * Return: Always 0 on success
  */
-int _islower(int c)
+int _islower(int r)
 {
-
+	if (r >= 97 && r <= 122)
+		_putchar('1');
+	else if (r >= 65 && r <= 91)
+		_putchar('0');
+	return (0);
 }
