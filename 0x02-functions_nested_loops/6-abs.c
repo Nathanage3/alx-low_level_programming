5305afa178aa6a5b48443845056040458525b9b8
Nathanage3 <sisay954@gmail.com>
2023-05-23 12:25:54 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,15 @@
 #include "main.h"
 /**
  * _abs - absolute value of a number
- * @c: the number to be evaluated
+ * @n: the number to be evaluated
  * Return: Always 0 on success and 1 for failure
  */
-int _abs(int)
+int _abs(int n)
 {
-
+	if (n > 0)
+		return (n);
+	else if (n == 0)
+		return (0);
+	else
+		return (-n);
 }
