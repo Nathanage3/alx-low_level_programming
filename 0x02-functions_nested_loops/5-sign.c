880b7265f7a25ee6ff91825b9f645406e3f51c58
Nathanage3 <sisay954@gmail.com>
2023-05-23 12:02:35 -0700

    sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,3 +5,20 @@
  * Return: Always 0 on success
  */
 int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
