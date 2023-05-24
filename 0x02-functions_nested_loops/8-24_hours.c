fd822e42fa6ec8c8ab57baf7de9ba3ef493dbc84
Nathanage3 <sisay954@gmail.com>
2023-05-23 18:43:42 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,21 @@
-#include <main.h>
+#include "main.h"
 /**
  * jack_bauer - void function
  */
 void jack_bauer(void)
 {
+	int i, j;
 
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
+			_putchar(':');
+			_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
+			_putchar('\n');
+		}
+	}
 }
