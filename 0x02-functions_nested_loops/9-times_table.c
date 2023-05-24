95f8e16d06ba34d6cdd23840ed9d9c065c679605
Nathanage3 <sisay954@gmail.com>
2023-05-23 19:28:47 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,23 +11,19 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			m = i * j;
-			if (m < 10)
+			if (j == 0)
 			{
-				_putchar(' ');
 				_putchar('0' + m);
-				if (j == 9)
-					break;
-				_putchar(',');
-				_putchar(' ');
 			}
 			else
 			{
-			_putchar('0' + (m / 10));
-			_putchar('0' + (m % 10));
-			if (j == 9)
-				break;
-			_putchar(',');
-			_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
+				if (m <= 9)
+					_putchar(' ');
+				else
+					_putchar('0' + (m / 10));
+				_putchar('0' + (m % 10));
 			}
 		}
 		_putchar('\n');
