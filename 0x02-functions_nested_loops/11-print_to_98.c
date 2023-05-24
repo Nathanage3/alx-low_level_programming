dd7d14b2b5cd670791739b089c76da61923d35d6
Nathanage3 <sisay954@gmail.com>
2023-05-23 19:49:01 -0700

    up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,4 +6,23 @@
  */
 void print_to_98(int n)
 {
+    if (n <= 98)
+    {
+        for (; n <= 98; n++)
+        {
+            printf("%d", n);
+            if (n != 98)
+                printf(", ");
+        }
+    }
+    else
+    {
+        for (; n >= 98; n--)
+        {
+            printf("%d", n);
+            if (n != 98)
+                printf(", ");
+        }
+    }
+    printf("\n");
 }
