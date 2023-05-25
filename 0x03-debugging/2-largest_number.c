70ac38795bdf9c64d649e5ecbca3d56e7a4c789c
Nathanage3 <sisay954@gmail.com>
2023-05-25 12:40:13 -0700

    fixing for the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
+	
+	if (a > b && a > c)
+	{
+		largest = a;
+    	}
+    	else if (b > a && b > c)
+	{
+        	largest = b;
+    	}
+    	else
+    	{
+        	largest = c;
+    	}
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
+    	return (largest);
 }
