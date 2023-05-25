2f68627661afa9d6e33bf8e4f111dc18282de51d
Nathanage3 <sisay954@gmail.com>
2023-05-25 00:37:34 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-	int n1 = 0, n2 = 1, n3, i;
+	long long int n1 = 0, n2 = 1, n3, i;
 
      	for (i = 2; i <= 50; i++)
      	{
 		n3 = n1 + n2;
-		printf("%d, ", n3);
+		printf("%lld, ", n3);
 		n1 = n2;
 		n2 = n3;
      	}
