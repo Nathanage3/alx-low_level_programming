0fbf658bcb77d07e7bf51e10c3ae62c545fc1044
Nathanage3 <sisay954@gmail.com>
2023-05-25 02:07:10 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - filtering uppercase letters
+ * @c: the ascii value sent to main function
+ * Return: Always 0 for success and return 1 for failure 
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
