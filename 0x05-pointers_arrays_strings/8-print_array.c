e4b0e0d812300bedb87cb19516cc48b4bc7bed94
Nathanage3 <sisay954@gmail.com>
2023-05-30 05:04:39 -0700

    print the value of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print the value of array
+ * @a: array
+ * @n: index of array
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d ", a[i]);
+		i++;
+	}
+	printf("\n");
+}
