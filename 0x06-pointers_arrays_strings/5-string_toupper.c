9bcdf965ce0d3953b88e01f785839cbce555510c
Nathanage3 <sisay954@gmail.com>
2023-06-01 09:32:57 -0700

    change to UPPER

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changing to upper
+ * @str: string to be tested
+ * Return: converted string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
