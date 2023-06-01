8c306cc1dad5d9537ecc9722f13a5363a6446602
Nathanage3 <sisay954@gmail.com>
2023-06-01 15:26:39 -0700

    print integer number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,19 +8,29 @@ void print_number(int n)
 {
 
 	if (n < 0)
+	{
 		_putchar('-');
 		n = -n;
+	}
 	if (n >= 10000)
+	{
 		_putchar('0' + n / 10000);
 		n %= 10000;
+	}
 	if (n >= 1000)
+	{
 		_putchar('0' + n / 1000);
 		n %= 1000;
+	}
 	if (n >= 100)
+	{
 		_putchar('0' + n / 100);
 		n %= 100;
+	}
 	if (n >= 10)
+	{
 		_putchar('0' + n / 10);
 		n %= 10;
+	}
 	_putchar('0' + n);
 }
