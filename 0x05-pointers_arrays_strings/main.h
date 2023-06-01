e227e69f12f849a5ac1bcca731522e80ba6aa939
Nathanage3 <sisay954@gmail.com>
2023-06-01 14:27:54 -0700

    magic in pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,11 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
+char *string_toupper(char *str);
 int _putchar(char c);
+char *cap_string(char *str);
+char string(char *str);
+char *rot13(char *);
+void print_number(int n);
+char *leet(char *str);
 #endif
