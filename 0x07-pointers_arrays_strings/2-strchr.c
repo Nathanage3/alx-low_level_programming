7d79bc55d85dd880128a92ff2c011f1edf5c9c0b
Nathanage3 <sisay954@gmail.com>
2023-06-05 13:11:55 -0700

    First occurence of a char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - first occurence of a character
+ * @s: the string
+ * @c: the char to be searched
+ * Return: pointer string
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == b)
+		{
+			return (s);
+		}
+		i++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
