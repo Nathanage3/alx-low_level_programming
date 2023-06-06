1ec798b91a9694d2762041f27b71c8796f41e402
Nathanage3 <sisay954@gmail.com>
2023-06-06 08:14:47 -0700

    first occureence

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,10 +21,10 @@ char *_strstr(char *haystack, char *needle)
 		{
 			return (&haystack[i]);
 		}
-		if (needle[j] == '\0')
-		{
-			return (&haystack[i]);
-		}
+	}
+	if (needle[j] == '\0')
+	{
+		return (&haystack[i]);
 	}
 	return (NULL);
 }
