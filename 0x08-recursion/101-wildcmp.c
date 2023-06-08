ec50c0eeb2e19f1d0f6157204629eb4936587f35
Nathanage3 <sisay954@gmail.com>
2023-06-08 10:56:15 -0700

    wild compare

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/***
+ * wildcmp - wild compare
+ * @s1: The first string
+ * @s2: The second string
+ * Return: Always 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1) == 1)
+		{
+			return (1);
+		}
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2) == 1)
+		{
+			return (1);
+		}
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
