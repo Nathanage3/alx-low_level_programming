6ba847151a4e5c3b6edd69854fa1c2bcc26c9752
Nathanage3 <sisay954@gmail.com>
2023-06-09 15:23:53 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 for success
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%s%d\n", argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+#endif
