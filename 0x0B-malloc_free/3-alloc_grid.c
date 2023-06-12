2e2e24c1a69815e244a5a612c3fe677366000be9
Nathanage3 <sisay954@gmail.com>
2023-06-12 15:01:53 -0700

    grid with 0 value

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,11 +22,6 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 				if (grid[i] == NULL)
 				{
-					for (j = 0; j < i; j++)
-					{
-						free(grid[j]);
-					}
-					free(grid);
 					return (NULL);
 				}
 				for (j = 0; j < width; j++)
