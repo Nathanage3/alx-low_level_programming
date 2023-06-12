a9a50264d9fdff1f0e406bc3adb74e641d6c98ff
Nathanage3 <sisay954@gmail.com>
2023-06-12 14:39:28 -0700

    allocate memory for grid with 0 value

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	grid = malloc(height * sizeof(int *));
+	grid = malloc(height * sizeof(int));
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
