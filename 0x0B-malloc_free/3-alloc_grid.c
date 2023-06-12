7865068689329ff329db010389ec665a3c6db545
Nathanage3 <sisay954@gmail.com>
2023-06-12 14:33:42 -0700

    allocate memory for grid with 0 value

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * alloc_grid - allocate memory for grid
+ * @width: width
+ * @height: height
+ * Return: Always 0 on success
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(height * sizeof(int *));
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+				if (grid[i] == NULL)
+				{
+					for (j = 0; j < i; j++)
+					{
+						free(grid[j]);
+					}
+					free(grid);
+					return (NULL);
+				}
+				for (j = 0; j < width; j++)
+				{
+					grid[i][j] = 0;
+				}
+	}
+	return (grid);
+}
