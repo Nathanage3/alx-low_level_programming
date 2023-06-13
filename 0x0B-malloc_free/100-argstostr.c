c50d58d6de2408a2c9a29fb4acf7eb516db898c7
Nathanage3 <sisay954@gmail.com>
2023-06-13 15:29:36 -0700

    pointer to argv with memory location

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * argstostr - argument string store
+ * @ac: argc
+ * @av: argv
+ * Return: Always 0 on success and 1 on failure
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, len, total_len = 0;
+	int current_pos = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		len = 0;
+		while (av[i][len])
+		{
+			len++;
+			total_len += len + 1;
+		}
+	}
+	str = malloc(sizeof(char) * (total_len + 1));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		len = 0;
+		while (av[i][len])
+		{
+			str[current_pos] = av[i][len];
+			len++;
+			current_pos++;
+		}
+		str[current_pos] = '\n';
+		current_pos++;
+	}
+	str[current_pos] = '\n';
+	return (str);
+}
