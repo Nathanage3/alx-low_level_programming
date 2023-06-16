dbe8bb4c7279ec06dd73a0f96c314e84e6962409
Nathanage3 <sisay954@gmail.com>
2023-06-16 06:52:09 -0700

    multiply

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+int _isdigit(char c);
+int _strlen(char *s);
+char *multiply(char *num1, char *num2);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
