9bffc3b470d55b1e558726ea5d131971dbe97815
Nathanage3 <sisay954@gmail.com>
2023-06-19 15:17:18 -0700

    trial

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,8 +14,28 @@ struct dog
 };
 
 typedef struct dog dog_t;
+char *_strcpy(char *dest, char *src)
+{
+    int i = 0;
 
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+
+    return dest;
+};
+int _strlen(char *str)
+{
+        int i = 0;
+
+        while (str[i] != '\0')
+        {i++; }
+        return (i);
+};
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
