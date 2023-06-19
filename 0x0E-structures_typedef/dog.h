bf3113953540289afb3eba817d788b5ac2f7e6a0
Nathanage3 <sisay954@gmail.com>
2023-06-19 16:29:09 -0700

    altogether

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
+
 char *_strcpy(char *dest, char *src);
 int _strlen(char *str);
-/**
- * struct dog - struct dog
- * @name: char pointer
- * @age: age of poppy
- * @owner: poppy owner
- */
+
 struct dog
 {
 	char *name;
@@ -16,7 +12,7 @@ struct dog
 };
 
 typedef struct dog dog_t;
-
+void free_dog(dog_t *d);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
