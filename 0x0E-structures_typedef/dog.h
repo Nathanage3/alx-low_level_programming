1ce587ff83548c4b491d290f5b1fddc129f33388
Nathanage3 <sisay954@gmail.com>
2023-06-19 12:51:04 -0700

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
+
+
 #define STRUCT_DOG
 struct dog
 {
@@ -8,4 +10,7 @@ struct dog
 	float age;
 	char *owner;
 }
+
+#define TYPEDEF_DOG
+typedef struct dog dog_t	
 #endif
