ac13bac8f7183005650015647c7a60e24a0cca38
Nathanage3 <sisay954@gmail.com>
2023-06-19 13:56:21 -0700

    initialilzing a function

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,13 @@ struct dog
 
 typedef struct dog dog_t;
 
+/**
+ * init_dog - Initializes a struct dog
+ * @d: Pointer to the struct dog to initialize
+ * @name: Pointer to the name string
+ * @age: Age of the dog
+ * @owner: Pointer to the owner string
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
