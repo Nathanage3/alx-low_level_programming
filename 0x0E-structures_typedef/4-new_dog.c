339474ef2a515cb4894626bfd58c7a5f42b571b2
Nathanage3 <sisay954@gmail.com>
2023-06-19 16:13:15 -0700

    4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,28 +1,40 @@
 #include <stdlib.h>
 #include "dog.h"
-/**
- * new_dog - poppy's friend
- * @name: new dog
- * @age: age of new dog in float
- * @owner: owner of new dog
- * Return: a pointer function value to the main function
- */
+int _strlen(char *str)
+{
+    int len = 0;
+
+    while (str[len] != '\0')
+        len++;
+    return (len);
+}
+char *_strcpy(char *dest, char *src)
+{
+    int i = 0;
+
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	int name_len;
+    dog_t *new_dog;
+    int name_len;
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-	{return (NULL); }
-	name_len = _strlen(name);
-	new_dog->name = malloc((name_len + 1) * sizeof(char));
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL); }
-	_strcpy(new_dog->name, name);
-	new_dog->age = age;
-	new_dog->owner = owner;
-	return (new_dog);
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return (NULL);
+    name_len = _strlen(name);
+    new_dog->name = malloc((name_len + 1) * sizeof(char));
+    if (new_dog->name == NULL) {
+        free(new_dog);
+        return (NULL); }
+
+    _strcpy(new_dog->name, name);
+    new_dog->age = age;
+    new_dog->owner = owner;
+    return (new_dog);
 }
