64f9e6ada244adb0a0e55220175176a11eb3aa45
Nathanage3 <sisay954@gmail.com>
2023-06-19 13:01:12 -0700

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - struct dog
+ * @name: char pointer
+ * @age: age of poppy
+ * @owner: poppy owner
+ */
 struct dog
 {
-	  
 	char *name;
 	float age;
 	char *owner;
