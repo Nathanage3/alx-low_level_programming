2538560f64f0759a0042b57d5d1020f56fd90e9b
Nathanage3 <sisay954@gmail.com>
2023-06-19 13:35:04 -0700

    initialize function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
+#include "dog.h"
 /**
  * init_dog - void function
  * @d: struct dog datatype
  * @name: char pointer
  * @age: age
  * @owner: owner
- */ 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	void *p;
-
-	(*p).age = 0;
-	(*p).name = "";
-	(*p).owner = "";
-	return (p);
+	d->age = age;
+	d->name = name;
+	d->owner = owner;
 }
