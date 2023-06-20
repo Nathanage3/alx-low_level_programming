1544ae8ec4a4f2d8d23228fb68f4d2f6d906f5e0
Nathanage3 <sisay954@gmail.com>
2023-06-20 16:46:05 -0700

    initialize function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,10 +8,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (*d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
