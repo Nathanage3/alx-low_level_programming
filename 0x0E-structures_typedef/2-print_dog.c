2f10151ad38b3cb82cc10aa6c229bf5a9a27c259
Nathanage3 <sisay954@gmail.com>
2023-06-20 16:32:14 -0700

    2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-		printf("Age: %.6f\n", d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 	}
 }
