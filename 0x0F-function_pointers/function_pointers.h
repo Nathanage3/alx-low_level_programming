5d63f7ef8dad488ea9ee38c7f28c46a8057d2bed
Nathanage3 <sisay954@gmail.com>
2023-06-21 13:41:41 -0700

    edited code

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,7 +3,7 @@
 #define FUNCTION_POINTERS_H_
 
 int _putchar(char c);
-void print_name(char *name, void (*f)(char *c));
+void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
