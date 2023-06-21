305923878de2379d93ee6b0e41f437691a4b5c47
Nathanage3 <sisay954@gmail.com>
2023-06-21 13:20:20 -0700

    my first commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,12 @@
+#include <stddef.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *c));
-void array_iterator(int *array, size_t size, void (*action)(int a));
-int int_index(int *array, int size, int (*cmp)(int a));
-int (*get_op_func(char *s))(int a, int b);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
