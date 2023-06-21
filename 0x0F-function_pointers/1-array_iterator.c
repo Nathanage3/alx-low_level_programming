c4ee0604ac9b2fe111cb3e5b57caf5686b894ef8
Nathanage3 <sisay954@gmail.com>
2023-06-21 13:58:53 -0700

    array ateration

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iteration - array
+ * @array: array
+ * @size: size of array
+ * @action: a pointer function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
