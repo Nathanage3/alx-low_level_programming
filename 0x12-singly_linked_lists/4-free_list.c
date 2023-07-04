d97ab02183ec9de12933c81886d665633096edf1
Nathanage3 <sisay954@gmail.com>
2023-07-04 11:44:41 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdllib.h>
+#include "lists.h"
+/**
+ * free_list
+ * @head:
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (h != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+		return (temp);
+	}
+}
