d5c700cf416ba27b1d8e92df70b5cb52a41f604f
Nathanage3 <sisay954@gmail.com>
2023-07-04 15:08:05 -0700

    add node int

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,11 +18,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (*head == NULL)
 		*head = newNode;
-	temp = *head;
-	while (temp->next != NULL)
+	else
 	{
-		temp = temp->next;
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = newNode;
 	}
-	temp->next = newNode;
 	return (newNode);
 }
