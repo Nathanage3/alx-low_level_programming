14acef4c5ecbc67fb5729b1549ea99e015d5c42f
Nathanage3 <sisay954@gmail.com>
2023-07-06 14:31:34 -0700

    endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * get_endiannes
+ * get_endianness - endianness
  * Return: 0 if beg endian, 1 if little endian
  */
 int get_endianness(void)
@@ -8,5 +8,5 @@ int get_endianness(void)
 	unsigned int val = 1;
 	char *p = (char *)&val;
 
-	return (int)*p;
+	return ((int)*p);
 }
