fcf78536da01316a36f9f76a01f05e23cdc98323
Nathanage3 <sisay954@gmail.com>
2023-07-14 16:47:15 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,14 @@
 #define MAIN_H
 
 #include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
 
+void copy_text(const char *from, const char *to);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
