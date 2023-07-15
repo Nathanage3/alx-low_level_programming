9a69e00e8c97e1da15be9ba8a4edae830034f797
Nathanage3 <sisay954@gmail.com>
2023-07-15 03:24:39 -0700

    fixed create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <sys/types.h>
 /**
  * create_file - create file
  * @filename: filename
@@ -8,25 +7,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int d, byte_w;
-	mode_t permissions = S_IRUSR | S_IWUSR;
+	int fd, byte_w;
+	int i = 0;
 
 	if (filename == NULL)
 		return (-1);
-	d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permissions);
-	if (d == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (*text_content != '\0')
-			text_content++;
-		byte_w = write(d, text_content, text_content - text_content);
-		if (byte_w == -1)
-		{
-			close(d);
-			return (-1);
-		}
+		while (text_content[i] != '\0')
+			i++;
 	}
-	close(d);
+	byte_w = write(fd, text_content, i);
+	if (byte_w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
 	return (1);
 }
