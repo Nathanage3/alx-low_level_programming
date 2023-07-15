642afff96dcbb8db66f309e3796f3f9f0bb6eae2
Nathanage3 <sisay954@gmail.com>
2023-07-15 05:19:53 -0700

    appened

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,24 +13,18 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, 1 | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
 	{
-		w = write(fd, NULL, 0);
-		return (-1);
-	}
-	else
-	{
-		while (text_content[i] != '\0')
-		{
-			i++; }
-		w = write(fd, text_content, i);
-		if (w == -1)
-			return (-1);
+		close(fd);
 		return (1);
 	}
-	close(fd);
+	while (text_content[i] != '\0')
+		i++;
+	w = write(fd, text_content, i);
+	if (w == -1)
+		return (-1);
 	return (1);
 }
