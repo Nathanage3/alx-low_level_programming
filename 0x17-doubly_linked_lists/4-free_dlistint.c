659fac2bba6fbebf4d0ab5cf7c2095bd3c21df62
nathanage3 <sisay954@gmail.com>
2023-09-14 16:49:59 -0700

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free-dlistint_t - free list
+ * @head: head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *actual_node;
+	dlistint_t *next_node;
+
+	if (head)
+	{
+		actual_node = head;
+		next_node = head->next;
+		while (next_node)
+		{
+			free(actual_node);
+			actual_node = next_node;
+			next_node = next_node->next;
+		}
+		free(actual_node);
+	}
+}
