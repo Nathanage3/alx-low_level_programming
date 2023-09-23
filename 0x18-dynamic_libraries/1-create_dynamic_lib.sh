53b7e18e96a95c0e92a90cd7fa284e283427d74f
nathanage3 <sisay954@gmail.com>
2023-09-22 17:17:31 -0700

    create dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c
-gcc -shared -o liball.so *.c
-rm -f *.o
 
+# Compile all .c files in the current directory into PIC object files
+gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c
+
+# Create the dynamic library liball.so from the PIC object files
+gcc -shared -o liball.so *.o
+
+# Clean up: remove the object files
+rm -f *.o
