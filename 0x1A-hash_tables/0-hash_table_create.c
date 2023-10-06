859b67969ef1e18d3dc46d660944916fe2c2da63
nathanage3 <sisay954@gmail.com>
2023-10-06 12:28:18 -0700

    edited

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,11 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
-	{
-		printf("Memory allocation failed\n");
-		free(hash_table);
 		return (NULL);
-	}
 	while (i < size)
 	{
 		hash_table->array[i] = NULL;
