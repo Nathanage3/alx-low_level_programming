13e3bef94f96f063ed9271c412a418dc88efaeea
nathanage3 <sisay954@gmail.com>
2023-10-06 07:50:33 -0700

    header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -37,4 +37,10 @@ typedef struct hash_table_s
 
 unsigned long int hash_djb2(const unsigned char *str);
 hash_table_t *hash_table_create(unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
 #endif
