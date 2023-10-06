e9d2844d53f4d9c8897fda7329142bfddf88f2de
nathanage3 <sisay954@gmail.com>
2023-10-06 13:11:54 -0700

    display the hashtable

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,36 +1,28 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_print - fucntion to print a hashtable
- * @ht: type pointer the hash_table
+ * hash_table_print - display
+ * @ht: hashtable
  */
-
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *current_node;
-	bool flag = false;
+	hash_node_t *node;
+	int comma_flag = 0;  /* To handle comma placement */
 
 	if (ht == NULL)
 		return;
-
 	printf("{");
+
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[i];
+		while (node != NULL)
 		{
-			if (flag == true)
+			if (comma_flag)
 				printf(", ");
-
-			for (current_node = ht->array[i]; current_node != NULL;
-				flag = true)
-			{
-				printf("'%s': '%s'", current_node->key,
-				       current_node->value);
-				current_node = current_node->next;
-				if (current_node != NULL)
-					printf(", ");
-			}
+			printf("'%s': '%s'", node->key, node->value);
+			comma_flag = 1;
+			node = node->next;
 		}
 	}
 	printf("}\n");
