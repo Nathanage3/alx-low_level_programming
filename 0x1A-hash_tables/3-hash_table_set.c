6015e9ecc2050442bfc368d3dd62230b35b81b8d
nathanage3 <sisay954@gmail.com>
2023-10-06 12:40:33 -0700

    setting the hashtable key/value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,3 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add an element at the hash table
+ * @ht: Type pointer to the hash table
+ * @key: Type const char pointer of the key
+ * @value: Value of the key
+ * Return: 0 if fail or 1 if success
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i, idx;
