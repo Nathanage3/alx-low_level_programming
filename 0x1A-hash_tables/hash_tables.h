8e8cf9ce0aab2feaef73471a9d80f4c0fbf347f4
nathanage3 <sisay954@gmail.com>
2023-10-06 07:41:18 -0700

    updated header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,5 +35,6 @@ typedef struct hash_table_s
      hash_node_t **array;
 } hash_table_t;
 
+unsigned long int hash_djb2(const unsigned char *str);
 hash_table_t *hash_table_create(unsigned long int size);
 #endif
